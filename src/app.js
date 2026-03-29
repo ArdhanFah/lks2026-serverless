a447b5041b9c462f0d6af08aa0a6232413d2f2af
ArdhanFah <101484883+ArdhanFah@users.noreply.github.com>
2026-03-29 23:32:37 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@
  * KONFIGURASI: Ganti API_URL dengan output Terraform (api_gateway_url)
  */
 
-const API_URL = "https://GANTI_DENGAN_API_GATEWAY_URL.execute-api.us-east-1.amazonaws.com/prod/orders";
+const API_URL = "https://nuzkkf60w2.execute-api.us-east-1.amazonaws.com/prod/orders";
 
 // ─── State ───────────────────────────────────────
 let items = [];
